69617dd9b2b194872934d3120defb9eef49223da
Anthony YOU <spleen85@proton.me>
2025-05-08 10:44:58 +0200

    Resolve bug, if email not registered, shows message 'Email not found'

--- server.py
+++ server.py
@@ -28,7 +28,11 @@ def index():
 
 @app.route('/showSummary', methods=['POST'])
 def showSummary():
-    club = [club for club in clubs if club['email'] == request.form['email']][0]
+    club = [club for club in clubs if club['email'] == request.form['email']]
+    if len(club) == 0:
+        flash('Email not found')
+    else:
+        club = club[0]
     return render_template('welcome.html', club=club, competitions=competitions)
 
 
